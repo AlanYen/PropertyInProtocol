614c6d3f6c324474c22844b2f4d3713c7d57ffc1
AlanYen <alan_yen@17life.com>
2017-08-01 14:39:04 +0800

    Initial Commit

---
+++ PropertyInProtocol/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PropertyInProtocol
+//
+//  Created by Alan Yen on 2017/8/1.
+//  Copyright © 2017年 Alan-App. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PropertyInProtocol/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PropertyInProtocol
+//
+//  Created by Alan Yen on 2017/8/1.
+//  Copyright © 2017年 Alan-App. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
